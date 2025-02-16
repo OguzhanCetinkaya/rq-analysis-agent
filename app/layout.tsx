99e83468a65f44cc4885bd25b8fc6083e838ee4a
Oguzhan Cetinkaya <oguzhancetinkaya@gmail.com>
2025-02-15 22:05:08 -0800

    new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const inter = Inter({ subsets: ["latin"] })
 const DynamicHeader = dynamic(() => import("./components/Header"), { ssr: false })
 
 export const metadata: Metadata = {
-  title: "Basic Next.js TypeScript App",
-  description: "A simple Next.js app with TypeScript",
+  title: "System Analyst Agent",
+  description: "System Analyst Agent",
 }
 
 export default function RootLayout({
@@ -23,18 +23,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://unpkg.com/react-pdf@5.7.2/dist/esm/Page/AnnotationLayer.css" />
-        <Script id="google-analytics">
-          {`
-            window.addEventListener('load', function() {
-              document.body.setAttribute('data-new-gr-c-s-check-loaded', 'true');
-              document.body.setAttribute('data-gr-ext-installed', '');
-            });
-          `}
-        </Script>
       </head>
       <body className={inter.className}>
         <DynamicHeader />
-        {children}
+        <main className="mainContent">
+          {children}
+        </main>
       </body>
     </html>
   )
